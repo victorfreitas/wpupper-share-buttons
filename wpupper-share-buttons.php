e6521efae58384c1503a0a374220c3b57a6b356a
victorfreitas <victorfreitas1@gmail.com>
2016-04-15 06:31:57 -0300

    Bug fixes

--- wpupper-share-buttons.php
+++ wpupper-share-buttons.php
@@ -5,7 +5,7 @@
 /*
 	Plugin Name: WPUpper Share Buttons
 	Plugin URI:  https://github.com/victorfreitas
-	Version:     2.6.0
+	Version:     2.6.1
 	Author:      WPUpper
 	Author URI:  https://github.com/victorfreitas
 	License:     GPL2
@@ -58,7 +58,7 @@ class WPUSB_App
      *
      * @var String
      */
-	const VERSION = '2.6.0';
+	const VERSION = '2.6.1';
 
 	/**
 	 * Initialize the plugin by setting localization, filters, and administration functions.
